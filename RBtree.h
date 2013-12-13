440dad7affdd4bdb1bbcb7d6413c9e02bf04c3d8
JinKoo Kang <jkkang@pentasecurity.com>
2013-12-13 19:14:51 +0900

    added binary tree functions

--- RBtree.h
+++ RBtree.h
@@ -1,15 +1,36 @@
 #ifndef _RBTREE_H_
 #define _RBTREE_H_
 
-#include <RBnode.h>
+struct node
+{
+	enum{ red, black } color;
+	int key_value;
+
+	node *left;
+	node *right;
+	node *parent;
+};
 
 class RBtree
 {
 public:
-	RBtree();
+	RBtree( int root_val );
 	~RBtree();
 
-	RBnode root;
+	void destroy_tree();
+	void insert( int key );
+	node *search( int key );
+	
+private:
+	node* root;
+
+	void destroy_tree( node* leaf );
+	void create_node( int key, node* parent, node* leaf );
+	void insert( int key, node* leaf );
+	node *search( int key, node *leaf );
+
+	void left_rotate();
+	void rb_insert();
 };
 
 #endif
